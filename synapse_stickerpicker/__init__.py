f2edc3315083f0ab0e9d1238946c2df399462736
dimhome <dimhome.sf@gmail.com>
2026-01-11 12:26:32 +0100

    4 - hotfix

--- synapse_stickerpicker/__init__.py
+++ synapse_stickerpicker/__init__.py
@@ -3,32 +3,21 @@ __author__ = 'mizhgun@gmail.com'
 
 import typing
 import copy
-import logging
 
 if typing.TYPE_CHECKING:
     from synapse.module_api import ModuleApi
-
-logger = logging.getLogger(__name__)
 
 
 class AppendStickerPickerData:
 
     def __init__(self, config=None, api: 'ModuleApi' = None):
         self._store = api.http_client.hs.datastores.main
-        self.stickerpicker_data = {
-            "stickerpicker": {
-                "content": {
-                    "type": "m.stickerpicker",
-                    "url": config['stickerpicker_url'],
-                    "name": "Stickerpicker",
-                    "creatorUserId": "",
-                    "data": {}
-                },
-                "sender": "",
-                "state_key": "stickerpicker",
-                "type": "m.widget",
-                "id": "stickerpicker"
-            }
+        self.widget_content = {
+            "type": "m.stickerpicker",
+            "url": config['stickerpicker_url'],
+            "name": "Stickerpicker",
+            "creatorUserId": "",
+            "data": {}
         }
         api.register_account_validity_callbacks(on_user_registration=self.on_user_registration)
 
@@ -37,9 +26,10 @@ class AppendStickerPickerData:
         return config
 
     async def on_user_registration(self, user: str) -> None:
-        logger.info("AppendStickerPickerData: on_user_registration called for user: %s", user)
-        data = copy.deepcopy(self.stickerpicker_data)
-        data['stickerpicker']['content']['creatorUserId'] = user
-        data['stickerpicker']['sender'] = user
-        logger.info("AppendStickerPickerData: saving data for user %s: %s", user, data)
-        await self._store.add_account_data_for_user(user, 'm.widgets', data)
+        content = copy.deepcopy(self.widget_content)
+        content['creatorUserId'] = user
+        account_data = {
+            "stickerpicker": content
+        }
+
+        await self._store.add_account_data_for_user(user, 'm.widgets', account_data)
